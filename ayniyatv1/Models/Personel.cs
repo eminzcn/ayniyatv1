ea6c7c63d6fa5fa0388b99def31e6bab517219d4
EminZCN <EminZCN@EMIN-OZCAN>
2023-07-18 17:55:22 +0300

    Modellerin oluşturulması.

---
+++ ayniyatv1/Models/Personel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ayniyatv1.Models
+{
+    public class Personel
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Adi { get; set; }
+
+        public string Soyadi { get; set; }
+        public string Birimi { get; set; }
+        public string Gorevi { get; set; }
+        public string Telefon { get; set;}
+        public int Dahili { get; set; }
+        public string Foto { get; set; }
+    }
+}
---
+++ ayniyatv1/Models/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ayniyatv1.Models
+{
+    public class Urun
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Tipi { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string SeriNumarasi { get; set; }
+        public string Foto { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
