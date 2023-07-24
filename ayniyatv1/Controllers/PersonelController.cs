f4b28b11ee0f87789a1d54cefa7a94f68c68b936
EminZCN <EminZCN@EMIN-OZCAN>
2023-07-24 14:56:03 +0300

    Controller oluşturulması.

---
+++ ayniyatv1/Controllers/PersonelController.cs
@@ -0,0 +1,22 @@
+﻿using ayniyatv1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ayniyatv1.Controllers
+{
+    public class PersonelController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public PersonelController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Personeller.ToList();
+            return View();
+        }
+    }
+}
---
+++ ayniyatv1/Controllers/UrunController.cs
@@ -0,0 +1,22 @@
+﻿using ayniyatv1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ayniyatv1.Controllers
+{
+    public class UrunController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public UrunController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Urunler.ToList();
+            return View();
+        }
+    }
+}
