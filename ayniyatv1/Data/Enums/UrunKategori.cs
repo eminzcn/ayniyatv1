a0b0a987242195adc78973818af5881566683284
EminZCN <EminZCN@EMIN-OZCAN>
2023-08-14 13:37:00 +0300

    Görsel ve İşlevsel Düzenlemeler

--- ayniyatv1/Data/Enums/UrunKategori.cs
+++ ayniyatv1/Data/Enums/UrunKategori.cs
@@ -1,20 +1,33 @@
-﻿namespace ayniyatv1.Data
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ayniyatv1.Data
 {
     public enum UrunKategori
     {
+        [Display(Name = "Dizüstü")]
         Dizustu = 1,
+        [Display(Name = "Bilgisayar Kasası")]
         Kasa,
+        [Display(Name = "Monitör")]
         Monitor,
         Fare,
+        [Display(Name = "Klavye")]
         Klavye,
         Kablo,
+        [Display(Name = "Çanta")]
         Canta,
         Sandalye,
+        [Display(Name = "Fotoğraf Makinası")]
         FotografMakinasi,
+        [Display(Name = "Yazıcı")]
         Yazici,
+        [Display(Name = "Tarayıcı")]
         Tarayici,
         Televizyon,
+        [Display(Name = "Uydu Alıcısı")]
         UyduAlicisi,
+        [Display(Name = "Diğer")]
         Diger
     }
 }
